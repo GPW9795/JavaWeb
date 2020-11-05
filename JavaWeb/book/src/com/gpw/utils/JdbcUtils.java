f78e25330a018cc2fcceb150038a4ea143e6cf45
gaopeiwen <vivian9795@163.com>
2020-11-05 16:48:31 +0800

    爱宝宝

---
+++ JavaWeb/book/src/com/gpw/utils/JdbcUtils.java
@@ -0,0 +1,22 @@
+package com.gpw.utils;
+
+import java.sql.Connection;
+
+public class JdbcUtils {
+    /**
+     * 获取数据库连接层中的连接
+     *
+     * @return
+     */
+    public static Connection getConnection() {
+        return null;
+    }
+
+    /**
+     * 关闭连接，放回数据库连接池
+     * @param connection
+     */
+    public static void close(Connection connection) {
+
+    }
+}
