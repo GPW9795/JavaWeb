7373170951140557d23008491e9ab6c0089f7462
gaopeiwen <vivian9795@163.com>
2020-11-15 22:36:34 +0800

    完成验证码模块的功能

--- JavaWeb/book/src/com/gpw/web/UserServlet.java
+++ JavaWeb/book/src/com/gpw/web/UserServlet.java
@@ -10,14 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY;
+
 public class UserServlet extends BaseServlet {
     UserService userService = new UserServiceImpl();
 
     /**
      * 处理登陆的操作
-     *
-     * @param req
-     * @param resp
      */
     protected void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
@@ -28,29 +27,38 @@ public class UserServlet extends BaseServlet {
             req.setAttribute("username", username);
             req.getRequestDispatcher("/pages/user/login.jsp").forward(req, resp);
         } else {
+            // 保存用户登录的信息到session
+            req.getSession().setAttribute("user", loginUser);
             req.getRequestDispatcher("/pages/user/login_success.jsp").forward(req, resp);
         }
     }
 
     /**
+     * 注销
+     */
+    protected void logout(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect(req.getContextPath());
+    }
+
+    /**
      * 处理注册的操作
-     *
-     * @param req
-     * @param resp
      */
     protected void regist(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 1 获取请求的参数
+        // 1 获取并删除验证码
+        String token = (String) req.getSession().getAttribute(KAPTCHA_SESSION_KEY);
+        req.getSession().removeAttribute(KAPTCHA_SESSION_KEY);
+        // 2 获取请求的参数
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
         String code = req.getParameter("code");
 
-
         User user = WebUtils.copyParamToBean(req.getParameterMap(), new User());
 
-        // 2 检查验证码是否正确，先写死
-        if ("abcd".equalsIgnoreCase(code)) { // 验证码正确
-            // 3 检查用户名是否可用
+        // 3 检查验证码是否正确，先写死
+        if (token != null && code.equalsIgnoreCase(token)) { // 验证码正确
+            // 4 检查用户名是否可用
             if (userService.existUsername(username)) { // 不可用
                 req.setAttribute("msg", "用户名已存在");
                 req.setAttribute("username", username);
